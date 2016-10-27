128c97d7adaeeb139dd6b2690f3667b11cce2696
Brittny Lapierre <brittny.lapierre@gmail.com>
2016-10-27 03:31:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,2 @@
 $( document ).ready(function(){
-	console.log("begin!");
-	$( "#menu-icon" ).click(function(){
-		console.log("show");
-		$( "#menu" ).css("width", "250px");
-		$( "#content" ).css("margin-left", "250px");
-	});
-
-	$( "#menu-close-icon" ).click(function(){
-		console.log("hide");
-		$( "#menu" ).css("width", "0");
-		$( "#content" ).css("margin-left", "0");
-	});
 });
