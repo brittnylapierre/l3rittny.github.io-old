9f52c66a2a0945f5d372f15d78862fe8afc5a49f
l3rittny <brittny.lapierre@gmail.com>
2016-10-28 17:52:12 -0400

    fixed website for all www

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,14 +1,2 @@
 $( document ).ready(function(){
-	console.log("begin!");
-	$( "#menu-icon" ).click(function(){
-		console.log("show");
-		$( "#menu" ).css("width", "250px");
-		$( "#personal-summary" ).css("margin-left", "250px");
-	});
-
-	$( "#menu-close-icon" ).click(function(){
-		console.log("hide");
-		$( "#menu" ).css("width", "0");
-		$( "#personal-summary" ).css("margin-left", "0");
-	});
 });
